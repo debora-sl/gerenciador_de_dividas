a8af33a0e5b6618df5f26a160de00f3cc074515d
debora-sl <debora.souzalima22@gmail.com>
2023-04-28 23:44:11 -0300

    atualizando HTML e adicioando dinanismo na página

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("main.js conectado");
+console.log("Esta um lindo dia para codar");
+
+
+const criaContas = ()=>{
+    //selecionando a tabela de contas
+    const tabelaContas = document.querySelector('[data-tabela-resultado]');
+    console.log(tabelaContas); 
+
+    //selecionando os campos conta e valor
+    const inputConta = document.querySelector('[data-input-nome]');
+    const contaNome = inputConta.value;
+
+    const inputValor = document.querySelector('[data-input-valor]');
+    const contaValor = inputValor.value;
+    console.log(contaNome);
+    console.log(contaValor); 
+
+   
+
+}
+
+// selecionando o botão, adicionando evento escutador de clique e chamando a função que imprime
+const botao = document.querySelector('[data-botao-adiciona-conta]');
+botao.addEventListener('click', criaContas);
